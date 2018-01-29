88e181e78eba8a37fd416f466394580d5a6f89ba
erainer <31553480+erainer@users.noreply.github.com>
2018-01-29 15:28:18 -0600

    Initial Commit

--- BasicInteration/ViewController.swift
+++ BasicInteration/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let title = sender.title(for: .selected)!
+        let text = "\(title) button pressed"
+        let styledText = NSMutableAttributedString(string:text)
+        let attributes = [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: statusLabel.font.pointSize)]
+        let nameRange = (text as NSString).range(of: title)
+        styledText.setAttributes(attributes, range: nameRange)
+        
+        statusLabel.attributedText = styledText
+    }
+    
+    @IBOutlet weak var statusLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
